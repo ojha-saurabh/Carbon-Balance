b9395fc5e318a7dda9a5775d4fea6c1ac7e3d4d1
Saurabh Kumar Ojha <m1073259@mindtree.com>
2021-09-07 16:14:21 +0530

    create profile and feeback

--- src/app/modules/pages/pages-routing.module.ts
+++ src/app/modules/pages/pages-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CalculateCarbonComponent } from './calculate-carbon/calculate-carbon.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 import { PagesComponent } from './pages.component';
 
-const routes: Routes = [{ path: 'caculate-co2', component: CalculateCarbonComponent }, { path: 'landing-page', component: LandingPageComponent }];
+const routes: Routes = [{ path: 'caculate-co2', component: CalculateCarbonComponent }, { path: 'landing-page', component: LandingPageComponent }, {path: 'create-profile', component: CreateProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -5,10 +5,11 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { CalculateCarbonComponent } from './calculate-carbon/calculate-carbon.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
 
 
 @NgModule({
-  declarations: [PagesComponent, CalculateCarbonComponent, LandingPageComponent],
+  declarations: [PagesComponent, CalculateCarbonComponent, LandingPageComponent, CreateProfileComponent],
   imports: [
     CommonModule,
     PagesRoutingModule
